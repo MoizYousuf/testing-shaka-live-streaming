5c076f6a5f15b656c02d103ea549fc3632efd1d0
MoizYousuf <moizyousuf24@gmail.com>
2020-11-19 20:05:12 +0500

    setup some

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
 	mongoURI: 'mongodb+srv://am:1234@cluster0.hgo6e.mongodb.net/ShakaSports?retryWrites=true&w=majority',
-	secretOrKey: process.env.SECRECT_OR_KEY
+	secretOrKey:'secret'
 };
