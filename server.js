846fe9770b82c3bc6707780beb50ae2e66a06ece
MoizYousuf <moizyousuf24@gmail.com>
2020-11-20 11:09:53 +0500

    mazakh nhi

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ app.get("/", function (req, res, next) {
 });
 
 // Set up a port
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port: ${port}`)); 
