bd6a1a7ec83e913f297deb3b12388e80848ba30f
MoizYousuf <moizyousuf24@gmail.com>
2020-11-26 19:45:12 +0500

    change server

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,6 @@
 module.exports = {
-	mongoURI: 'mongodb+srv://am:1234@cluster0.hgo6e.mongodb.net/ShakaSports?retryWrites=true&w=majority',
+	// mongoURI: 'mongodb+srv://am:1234@cluster0.hgo6e.mongodb.net/ShakaSports?retryWrites=true&w=majority',
+	mongoURI: 'mongodb+srv://user:1234@cluster0.a3ibg.mongodb.net/app?retryWrites=true&w=majority',
 	secretOrKey: 'secret'
 	// mongoURI: 'mongodb://localhost:27017/gymChalen',
 
