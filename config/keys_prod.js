4a154c5566e0a342ed2bcbb8e1666c4784011c5e
MoizYousuf <moizyousuf24@gmail.com>
2021-01-14 15:16:25 +0500

    me when i have data and i tell to my friend that i have data. my friends be like: Bhai nhi hy please hotspot dena

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,8 @@
 module.exports = {
-	mongoURI: 'mongodb+srv://am:1234@cluster0.hgo6e.mongodb.net/ShakaSports?retryWrites=true&w=majority',
-	secretOrKey:'secret'
+  mongoURI:
+    "mongodb+srv://am:1234@cluster0.hgo6e.mongodb.net/ShakaSports?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+  username: "09db8235-9ccd-416e-b59f-7709b8829041",
+  password:
+    "ApQmskcBzQSrY6VLV3gwZh1GCPyMve5wAgU4AV52GjbCtrRjQpGF9ynMNe7KqcAJdHfR3tG0lXW",
 };
