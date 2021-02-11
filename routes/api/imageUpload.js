e25940ebee76525f4e912e242c4c9ba4deb703f0
MoizYousuf <moizyousuf24@gmail.com>
2021-02-11 14:59:48 +0500

    its time to upload

--- routes/api/imageUpload.js
+++ routes/api/imageUpload.js
@@ -1,7 +1,24 @@
 const multer = require("multer");
-const upload = multer({ dest: __dirname + "/uploads/images" });
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 router.post("/", upload.single("photo"), async function (req, res) {
   if (req.file) {
